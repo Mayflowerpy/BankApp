690620a5314d7f19aeb4de6baab306c9888b428e
Vladislav Shilov <Shilovvlad@mail.ru>
2023-02-03 19:57:44 +0300

    revertProfileService

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -2,11 +2,7 @@ package com.bank.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
-@EnableFeignClients
 @SpringBootApplication
 public class ProfileApplication {
     public static void main(String[] args) {
