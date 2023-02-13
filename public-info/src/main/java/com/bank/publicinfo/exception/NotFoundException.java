5ceb6f858da9155302c852c5f60dd301db2eee4e
UnsleepingOwl <lion2000_07@mail.ru>
2023-02-13 15:33:34 +0300

    Исправление недостатков кода

--- public-info/src/main/java/com/bank/publicinfo/exception/NotFoundException.java
+++ public-info/src/main/java/com/bank/publicinfo/exception/NotFoundException.java
@@ -4,17 +4,9 @@ package com.bank.publicinfo.exception;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-/**
- * NotFoundException - исключение, выбрасываемое при отсутствии искомого объекта
- *
- * @author UnsleepingOwl (Lev Yakolin)
- */
-
 @Getter
 @NoArgsConstructor
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException(String message) {
-        super(message);
-    }
+    private String message;
 }
