f856480740f14e8de766efff7b55acacafc1efe0
Dina <dinapetrashova@gmail.com>
2023-02-15 18:03:32 +0000

    account_base_structure

---
+++ account/src/main/java/com/bank/account/exception/AccountNotFoundException.java
@@ -0,0 +1,15 @@
+package com.bank.account.exception;
+
+
+/**
+ * Нестандартный класс-исключение для обработки запроса с несуществующим id
+ *
+ *  @author Dina Petrashova
+ *  @om 2023-02-07
+*/
+
+public class AccountNotFoundException extends RuntimeException{
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ account/src/main/java/com/bank/account/service/AuditService.java
@@ -0,0 +1,20 @@
+package com.bank.account.service;
+
+import com.bank.account.model.dto.AuditDTO;
+import com.bank.account.model.entity.Audit;
+import java.util.List;
+
+/**
+ * Интерфейс, содержащий методы CRUD-операций для сущности Account.
+ *
+ * @author Dina Petrashova
+ * @om 2023-02-01
+ */
+
+public interface AuditService {
+    List<Audit> getAll();
+    Audit getByID(Long id);
+    void save (AuditDTO auditDTO);
+    void update (AuditDTO auditDTO, Long id);
+    void delete (Long id);
+}
